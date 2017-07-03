927a9fe43e27652f57be5f4344b28d850995f83a
maciek <m.p.dutkiewicz@gmail.com>
2017-07-03 18:24:49 +0200

    nowy

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// zwykły opis 1
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name); //koment do alert
+/*
+długi
+koment
+bez wyłączenia kodu
+*/
+// blablabl 3
+console.log('Hello, ' + name);
